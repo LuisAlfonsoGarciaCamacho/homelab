42c20426e227395f7154bacd8d83df9ccae7e4e1
LuisAlfonsoGarciaCamacho <luisg.camachoda@gmail.com>
2026-02-08 04:44:58 -0600

    feat: added another dag

--- airflow/dags/test_simple_dag_2.py
+++ airflow/dags/test_simple_dag_2.py
@@ -3,7 +3,7 @@ from datetime import datetime
 
 
 @dag(schedule=None, start_date=datetime(2026, 2, 1), catchup=False, tags=["test"])
-def test_simple_dag():
+def test_simple_dag_2():
     @task
     def get_value():
         return "hello from test 2"
@@ -16,4 +16,4 @@ def test_simple_dag():
     print_value(get_value())
 
 
-test_simple_dag()
+test_simple_dag_2()
