f26c1060f1595b45b83504e78f461b09804381d1
LuisAlfonsoGarciaCamacho <luisg.camachoda@gmail.com>
2026-02-08 04:24:00 -0600

    feat: added sync dags

---
+++ airflow/dags/test_simple_dag.py
@@ -0,0 +1,19 @@
+from airflow.decorators import dag, task
+from datetime import datetime
+
+
+@dag(schedule=None, start_date=datetime(2026, 2, 1), catchup=False, tags=["test"])
+def test_simple_dag():
+    @task
+    def get_value():
+        return "hello"
+
+    @task
+    def print_value(msg: str):
+        print(msg)
+        return msg
+
+    print_value(get_value())
+
+
+test_simple_dag()
